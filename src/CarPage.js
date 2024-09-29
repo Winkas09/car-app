a8494239ba2292f3f123f7eb382509461af04ee4
Edvinas Pasinas <edvinas17935@gmail.com>
2024-09-29 19:23:08 +0300

    added context basic ?

--- src/CarPage.js
+++ src/CarPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import CarForm from "./CarForm";
 import CarList from "./CarList";
+import { CarContext } from "./CarContext";
 
 const CarPage = () => {
   const [cars, setCars] = useState([]);
@@ -8,12 +9,20 @@ const CarPage = () => {
   const addCar = (car) => {
     setCars([...cars, car]);
   };
+
+  const ctxValue = {
+    cars,
+    addCar,
+  };
+
   return (
+    <CarContext.Provider value={ctxValue}>
     <div>
       <h1>Car Page</h1>
       <CarForm addCar={addCar} />
       <CarList cars={cars} />
     </div>
+    </CarContext.Provider>
   );
 };
 

3748be7f25cd62a2db519452a614ffc1466c3f8e
Edvinas Pasinas <edvinas17935@gmail.com>
2024-10-01 22:29:30 +0300

    adjustments

--- src/api-project/ApiItem.js
+++ src/api-project/ApiItem.js
@@ -5,7 +5,7 @@ const ApiItem = ({ item, type }) => {
   switch (type) {
     case 'user':
       return (
-        <div>
+        <div className='api-item'>
           <h2>{item.name}</h2>
           <p>Email: {item.email}</p>
           <p>Phone: {item.phone}</p>
@@ -15,14 +15,14 @@ const ApiItem = ({ item, type }) => {
       );
     case 'album':
       return (
-        <div>
+        <div className='api-item'>
           <h2>{item.title}</h2>
             <Link to={`/album/${item.id}`}>View Photos</Link>
         </div>
       );
     case 'post':
       return (
-        <div>
+        <div className='api-item'>
           <h2>{item.title}</h2>
           <p>{item.body}</p>
             <Link to={`/post/${item.id}`}>View Details</Link>   
--- src/api-project/ProjectHomePage.js
+++ src/api-project/ProjectHomePage.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ProjectHomePage = () => {
   return (
-    <div>
+    <div className='home-nav'>
       <h1>Welcome to the Project Home Page</h1>
       <p>Select a category to view details:</p>
       <ul>
-        <li><a href="/project/users">Users</a></li>
-        <li><a href="/project/posts">Posts</a></li>
-        <li><a href="/project/albums">Albums</a></li>
+        <li><Link to="/project/users">Users</Link></li>
+        <li><Link to="/project/posts">Posts</Link></li>
+        <li><Link to="/project/albums">Albums</Link></li>
       </ul>
     </div>
   );
